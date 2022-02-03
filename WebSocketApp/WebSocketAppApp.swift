aada1c5baeb7c4f138487c8b8c87c12a53d070d9
Marc Orel <marc1.orel@outlook.de>
2022-02-03 19:27:40 +0100

    Initial Commit

---
+++ WebSocketApp/WebSocketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketAppApp.swift
+//  WebSocketApp
+//
+//  Created by Marc Orel on 03.02.22.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
